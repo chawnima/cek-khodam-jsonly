ebb78990b30b2913debe934fb897dcdd915dcce7
chawnima <mferdyalviansyah@gmail.com>
2024-07-27 07:04:31 +0700

    1.12

--- script.js
+++ script.js
@@ -2,52 +2,70 @@ const input = document.getElementById("input");
 const output = document.getElementById("output");
 const submit = document.getElementById("submit");
 const khodam = [
-  { name: "Serigala Sumatera", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Tuyul Crypto", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Jenglonte", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Buto Hitam", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Jawir Lv 69", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Kuyang User Fanny", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Banteng Merah", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Bocil Dirot", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Genderuwo Gaming", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Mega Chan", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Windah Hitam", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Jong Koooook", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Kontolodon", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Wibu Tokrev", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Willie Salim", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Macan Terbang", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Bwa Bwa Bwa", rarity: '<span class="onestar">★ (48%)</span>' },
-  { name: "Crazy Killer", rarity: '<span class="onestar">★ (48%)</span>' },
+  { name: "Serigala Sumatera", rarity: 1 },
+  { name: "Tuyul Crypto", rarity: 1 },
+  { name: "Jenglonte", rarity: 1 },
+  { name: "Buto Hitam", rarity: 1 },
+  { name: "Jawir Lv 69", rarity: 1 },
+  { name: "Kuyang User Fanny", rarity: 1 },
+  { name: "Banteng Merah", rarity: 1 },
+  { name: "Bocil Dirot", rarity: 1 },
+  { name: "Genderuwo Gaming", rarity: 1 },
+  { name: "Mega Chan", rarity: 1 },
+  { name: "Windah Hitam", rarity: 1 },
+  { name: "Jong Koooook", rarity: 1 },
+  { name: "Kontolodon", rarity: 1 },
+  { name: "Wibu Tokrev", rarity: 1 },
+  { name: "Willie Salim", rarity: 1 },
+  { name: "Macan Terbang", rarity: 1 },
+  { name: "Bwa Bwa Bwa", rarity: 1 },
+  { name: "Crazy Killer", rarity: 1 },
 
-  { name: "Ambatron", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Kucing Tanpa Biji", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Alomani Hitam", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Genderuwo Pedo", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Alok Hamil", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Salamander Samarinda", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Member SUKI", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Skibidi Toilet", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Maling Sempak", rarity: '<span class="twostar">★★ (25%)</span>' },
-  { name: "Admin SUKI", rarity: '<span class="twostar">★★ (25%)</span>' },
+  { name: "Ambatron", rarity: 2 },
+  { name: "Kucing Tanpa Biji", rarity: 2 },
+  { name: "Alomani Hitam", rarity: 2 },
+  { name: "Genderuwo Pedo", rarity: 2 },
+  { name: "Alok Hamil", rarity: 2 },
+  { name: "Salamander Samarinda", rarity: 2 },
+  { name: "Member SUKI", rarity: 2 },
+  { name: "Skibidi Toilet", rarity: 2 },
+  { name: "Maling Sempak", rarity: 2 },
+  { name: "Admin SUKI", rarity: 2 },
 
-  { name: "Laba-laba Sunda", rarity: '<span class="threestar">★★★ (17.5%)</span>' },
-  {name: "Penunggu Kapal Karam Pontianak",rarity: '<span class="threestar">★★★ (17.5%)</span>'}, 
-  { name: "Kodok Zuma", rarity: '<span class="threestar">★★★ (17.5%)</span>' },
-  { name: "Yo Ndak Tau Kok Tanya Saya",rarity:'<span class="threestar">"★★★ (17.5%</span>'},
-  { name: "Raja Pedo", rarity: '<span class="threestar">★★★ (17.5%)</span>' },
-  { name: "Zeus Gacor", rarity: '<span class="threestar">★★★ (17.5%)</span>' },
-  { name: "Kuntilanak Tobrut", rarity: '<span class="threestar">★★★ (17.5%)</span>' },
+  { name: "Laba-laba Sunda", rarity: 3 },
+  { name: "Penunggu Kapal Karam Pontianak", rarity: 3 },
+  { name: "Kodok Zuma", rarity: 3 },
+  { name: "Yo Ndak Tau Kok Tanya Saya", rarity: 3 },
+  { name: "Raja Pedo", rarity: 3 },
+  { name: "Zeus Gacor", rarity: 3 },
+  { name: "Kuntilanak Tobrut", rarity: 3 },
 
-  {
-    name: "Tengkorak Bawa Biola",rarity: '<span class="fourstar">★★★★ (10%)</span>' },
-    { name: "Loli Beranak 4", rarity: '<span class="fourstar">★★★★ (10%)</span>' },
-  { name: "Stalin Tobrut", rarity: '<span class="fourstar">★★★★ (10%)</span>' },
-  { name: "Hitler Loli", rarity: '<span class="fourstar">★★★★ (10%)</span>' },
+  { name: "Tengkorak Bawa Biola", rarity: 4 },
+  { name: "Loli Beranak 4", rarity: 4 },
+  { name: "Stalin Tobrut", rarity: 4 },
+  { name: "Hitler Loli", rarity: 4 },
 
-  { name: "Sugiono", rarity: `<span class="fivestar">★★★★★ (2.5%)` },
+  { name: "Sugiono", rarity: 5 },
 ];
+const star1 = `<span class="onestar">★ (45%)</span>`,
+  star2 = `<span class="twostar">★★ (25%)</span>`,
+  star3 = `<span class="threestar">★★★ (17.5%)</span>`,
+  star4 = `<span class="fourstar">★★★★ (10%)</span>`,
+  star5 = `<span class="fivestar">★★★★★ (2.5%)</span>`;
+const starCount = (rarity) => {
+  switch (rarity) {
+    case 1:
+      return star1;
+    case 2:
+      return star2;
+    case 3:
+      return star3;
+    case 4:
+      return star4;
+    case 5:
+      return star5;
+  }
+};
 const filter = (text) => {
   return /[^a-zA-Z\s]/g.test(text);
 };
@@ -62,8 +80,15 @@ submit.addEventListener("click", () => {
   if (!localStorage.getItem(`khodam${input.value}`)) {
     const value = Math.floor(Math.random() * khodam.length);
     localStorage.setItem(`khodam${input.value}`, value);
-    output.innerHTML = `Khodam ${input.value} adalah ${khodam[value].name} <span class="container-star">${khodam[value].rarity}</span>`;
+    output.innerHTML = `Khodam ${input.value} adalah ${
+      khodam[value].name
+    } <span class="container-star">${starCount(khodam[value].rarity)}</span>`;
   } else {
-    output.innerHTML = `Khodam ${input.value} adalah ${khodam[localStorage.getItem(`khodam${input.value}`)].name} <span class="container-star">${khodam[localStorage.getItem(`khodam${input.value}`)].rarity}</span>`;
+    output.innerHTML = `Khodam ${input.value} adalah ${
+      khodam[localStorage.getItem(`khodam${input.value}`)].name
+    } <span class="container-star">${starCount(
+      khodam[localStorage.getItem(`khodam${input.value}`)].rarity
+    )}</span>`;
   }
 });
+
